ba8cef0ff605e95d0a66457507f2571120988744
Hunter Bacot <hsbacot@gmail.com>
2012-12-12 01:50:58 -0500

    pages and layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Bookmarks::Application.routes.draw do
+ 
+
+  root :to => 'static_pages#home'
+  get "about" => "static_pages#about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +54,8 @@ Bookmarks::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+
+  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
