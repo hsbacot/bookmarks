9e994a4f7e2e254563e9f382b20dbbe55d20f8a5
Hunter Bacot <hsbacot@gmail.com>
2012-12-12 00:22:20 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Bookmarks by Hunter Bacot
+
+++++++++++++++++++++++++++
+
+An application that allows user to store bookmarks to a central location and access them remotely
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookmarks::Application.config.secret_token = 'a98bc0eecbcccae1dfb6025aa9afd635dcb5e93ef7213ae66690596073b25abdbd5d387927cb0458d7aec7cf3f21bb083223653e2917cf8eaa5237854be1f9b7'
