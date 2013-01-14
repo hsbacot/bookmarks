6ed4928af245efb5fbd7888478eef2248efdfe97
Hunter Bacot <hsbacot@gmail.com>
2013-01-14 01:19:43 -0500

    usernam unique & size validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,5 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username
   # attr_accessible :title, :body
 
-  validates_presence_of :username
+  validates :username, :presence => true, :uniqueness => true, :length => { :minimum => 3, :maximum => 30 }
 end
